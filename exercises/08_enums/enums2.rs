0f97400ae34d052d59d49d2ce51e2c3f56d65e3e
Phan Huy Hoang <hoangph271@gmail.com>
2025-04-10 22:46:16 +0700

    wip

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -6,7 +6,11 @@ struct Point {
 
 #[derive(Debug)]
 enum Message {
-    // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(usize, usize, usize),
+    Quit,
 }
 
 impl Message {
